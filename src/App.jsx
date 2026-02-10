a6efff92832b91498c7155c0114053cf85a0fefc
allbutone <allbutone@foxmail.com>
2026-02-10 11:38:42 +0800

    抽离 Heading 组件, 基于 styled component 添加自定义样式, 并测试 props.as 的作用

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,10 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
 // styled components 借助了 es6 的 feature: tagged template literals
 // 当使用 H1 时, 会自动执行 styled.h1 这个 function, H1 就是这个 function 返回的 component
-const H1 = styled.h1`
-  font-size: 25px;
-  font-weight: bold;
-  text-decoration: underlidne;
-`;
 const StyledApp = styled.div`
   background-color: white;
   padding: 10px;
@@ -21,7 +17,9 @@ function App() {
       {/* 且其下不接受任何 children */}
       <GlobalStyles />
       <StyledApp>
-        <H1>hello world</H1>
+        <Heading as={"h1"}>heading of level 1</Heading>
+        <Heading as={"h2"}>heading of level 2</Heading>
+        <Heading as={"h3"}>heading of level 3</Heading>
         <Button onClick={() => alert("clicked!")}>LEFT</Button>
         <Button onClick={() => alert("clicked!")}>RIGHT</Button>
         <Input />
---
+++ src/ui/Heading.jsx
@@ -0,0 +1,37 @@
+import styled, { css } from "styled-components";
+
+// 如果不借助 helper function styled.h1, template literal 内的 ${fn}
+// 会被这么处理: fn.toString()
+// const Heading = `
+//   font-size: ${() => 10 > 5 ? '30px' : '10px'};
+// `;
+// 借助 helper function styled.h1, template literal 内的 ${fn}
+// 会被这么处理: fn() 得到 string, 然后嵌入到 template literal 内对应位置
+// const Heading = styled.h1`
+//   font-size: ${() => 10 > 5 ? '30px' : '10px'};
+// `;
+
+// styled components 提供了 props.as, 作用是: 将接收自定义样式的 element(h1) 动态替换为 props.as 指定的 element
+// return css`template_literal` 而不是 return `template_literal` 的原因是:
+// 1. 前者的 template_literal 内如果指定了 ${fn} 的话, fn 会被执行, 而不会被 .toString()
+// 2. 前者的 template_literal 内支持自动补全
+const Heading = styled.h1`
+  ${(props) => {
+    switch (props.as) {
+      case "h1":
+        return css`
+          font-size: 30px;
+        `;
+      case "h2":
+        return css`
+          font-size: 25px;
+        `;
+      case "h3":
+        return css`
+          font-size: 20px;
+        `;
+    }
+  }}
+`;
+
+export default Heading;
