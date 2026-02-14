24a55658b2207f637669af94df2da0d30101a064
allbutone <allbutone@foxmail.com>
2026-02-14 16:37:58 +0800

    test: 创建 supabase storage 后, 测试里面的 cabin image 是否可以正常加载

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -5,12 +5,13 @@ import { getCabins } from "../services/cabinOperations";
 
 function Cabins() {
   useEffect(() => {
-    getCabins().then(cabins => console.log(cabins));
-  }, [])
+    getCabins().then((cabins) => console.log(cabins));
+  }, []);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://zzudlfaityyrmtxwajxy.supabase.co/storage/v1/object/sign/cabin-images/cabin-001.jpg?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9mMmFlYzY2OC03ODVkLTQ0ZDEtYTNmNi01MmUzZjNkZWM3ODIiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJjYWJpbi1pbWFnZXMvY2FiaW4tMDAxLmpwZyIsImlhdCI6MTc3MTA1ODE0NiwiZXhwIjoxODAyNTk0MTQ2fQ.1UXMRz0f0DsvzPP_p-Q0YKuOG3DT3B_ibVTC6ezHWbg" />
     </Row>
   );
 }
