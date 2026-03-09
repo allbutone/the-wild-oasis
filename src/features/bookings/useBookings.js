71c0a9dcfdb457015faa3747b56185fd44ed1be3
allbutone <allbutone@foxmail.com>
2026-03-09 14:27:54 +0800

    fix: 如果 current page 已经是 final page, 就不要 prefetch data of next page 了

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -19,16 +19,6 @@ export default function useBookings() {
   const [field, direction] = sortBy.split("-");
   const sort = { field, direction };
 
-  //prefetching for next page:
-  //如果要与查询的数据已经有了, 可以直接 queryClient.setQueryData
-  const queryClient = useQueryClient();
-  //prefetchQuery() 会 return promise (promise to get data of next page)
-  //但不要在 prefetchQuery() 前面添加 await, 否则会耽误 data of current page 的获取
-  queryClient.prefetchQuery({
-    queryKey: ["bookings", filter, sort, pageNext],
-    queryFn: () => getBookings(filter, sort, pageNext),
-  });
-
   const {
     isLoading,
     data: { data: bookings, count } = {}, // data 在初期为 undefined, 因此需要指定为 {}, 防止解构报错
@@ -38,6 +28,20 @@ export default function useBookings() {
     queryKey: ["bookings", filter, sort, pageCurrent], // 建议在 react query devtools 中观察 key 值
     queryFn: () => getBookings(filter, sort, pageCurrent),
   });
+
+  //prefetching for next page:
+  const queryClient = useQueryClient();
+  //prefetchQuery() 会 return promise (promise to get data of next page)
+  //但不要在 prefetchQuery() 前面添加 await, 否则会耽误 data of current page 的获取
+  const pageCount = Math.ceil(count / size);
+  //如果当前页已经是最后一页, 就不要预载下一页数据了
+  if (page < pageCount) {
+    //如果要与查询的数据已经有了, 可以直接 queryClient.setQueryData
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sort, pageNext],
+      queryFn: () => getBookings(filter, sort, pageNext),
+    });
+  }
 
   return {
     isLoading,
