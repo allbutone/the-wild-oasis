ff9d4ac84988c5e9c1f19c5b0a90c3ef55caee0d
allbutone <allbutone@foxmail.com>
2026-03-24 17:56:06 +0800

    feat: 让 app theme 跟随 system theme

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -4,14 +4,17 @@ import { useLocalStorageState } from "../hooks/useLocalStorageState";
 const ThemeContext = createContext();
 
 export function ThemeContextProvider({ children }) {
-  const [isDarkMode, setIsDarkMode] = useLocalStorageState("isDarkMode", false); // 默认采用 light mode
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(
+    "isDarkMode",
+    window.matchMedia("(prefers-color-scheme: dark)").matches,
+  );
 
   // 当 theme 发生变化时, 改变 <html> 的样式:
   useEffect(() => {
     if (isDarkMode) {
       document.documentElement.classList.remove("light-mode");
       document.documentElement.classList.add("dark-mode");
-    }else{
+    } else {
       document.documentElement.classList.remove("dark-mode");
       document.documentElement.classList.add("light-mode");
     }
